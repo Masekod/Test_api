248725d5600b3a59bb71f205bc5ecf96d0a9789d
Vergilian <sparkmart@mail.ru>
2025-08-26 05:01:21 +0300

    correct

--- data/login_data.py
+++ data/login_data.py
@@ -3,17 +3,3 @@ def success_login_data():
         "email": "bingo@mail.ru",
         "password": "321Ytrewq",
     }
-
-
-# def incorrect_email_data():
-#     return {
-#         "email": "bingo123123@mail.ru",
-#         "password": "321Ytrewq321",
-#     }
-#
-#
-# def incorrect_password_login():
-#     return {
-#         "email": "bingo123123@mail.ru",
-#         "password": "321Ytrewq321",
-#     }
