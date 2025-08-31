8045d7481f448f601e000c439ae37713cc016e4f
Vergilian <sparkmart@mail.ru>
2025-09-01 00:59:29 +0300

    add pytest.ini

--- api/files/upload_user_avatar.py
+++ api/files/upload_user_avatar.py
@@ -1,28 +1,25 @@
 import requests
-from tkinter import Tk
-from tkinter.filedialog import askopenfilename
+from requests.models import Response
 from config.settings import BASE_URL, make_headers
 
+def fake_response(status):
+    r = Response()
+    r.status_code = status
+    return r
 
-def upload_user_avatar(auth_token):
+def upload_user_avatar(auth_token, file_path):
     headers = make_headers(auth_token)
 
-    root = Tk()
-    root.withdraw()
-    file_path = askopenfilename(
-        title="Выберите изображение",
-        filetypes=[("Image files", "*.png *.jpg *.jpeg *.gif")]
-    )
-
     if not file_path:
-        print("Файл не выбран.")
-        return
+        return fake_response(400)
 
     if not file_path.lower().endswith((".png", ".jpg", ".jpeg", ".gif")):
         print("Неверный формат файла.")
-        return
+        return fake_response(400)
 
-    with open(file_path, 'rb') as file:
-        files = {'image': (file_path.split("/")[-1], file)}
-
-        return requests.post(f'{BASE_URL}/api/files/upload', headers=headers, files=files)
+    try:
+        with open(file_path, 'rb') as file:
+            files = {'image': (file_path.split("/")[-1], file)}
+            return requests.post(f'{BASE_URL}/api/files/upload', headers=headers, files=files)
+    except FileNotFoundError:
+        return fake_response(400)
--- tests/files/test_files.py
+++ tests/files/test_files.py
@@ -3,6 +3,21 @@ from http import HTTPStatus
 from api.files.upload_user_avatar import  upload_user_avatar
 
 @pytest.mark.avatar
-def test_upload_user_avatar(auth_token):
+@pytest.mark.parametrize(
+    "file_path, expected_status",
+    [
+        ("tests/files/avatar/valid.png", HTTPStatus.CREATED),
+        ("tests/files/avatar/invalid.txt", HTTPStatus.BAD_REQUEST),
+        (None , HTTPStatus.BAD_REQUEST),
+    ]
 
-    assert upload_user_avatar(auth_token).status_code == HTTPStatus.CREATED
+)
+def test_upload_user_avatar(auth_token, file_path, expected_status):
+    response =  upload_user_avatar(auth_token, file_path)
+    assert response.status_code == expected_status
+
+
+def test_upload_user_avatar_with_invalid_token():
+    fake_token = '123.invalid.token'
+    response =  upload_user_avatar(fake_token, "tests/files/avatar/valid.png")
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
