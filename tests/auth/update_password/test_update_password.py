59025f0a9a764f19add3c670149d8d02619beaf2
Vergilian <sparkmart@mail.ru>
2025-09-07 15:48:20 +0300

    correct

--- tests/auth/update_password/test_update_password.py
+++ tests/auth/update_password/test_update_password.py
@@ -2,7 +2,6 @@ import allure
 import pytest
 from http import HTTPStatus
 from config.settings import NEW_PASSWORD,INVALID_PASSWORD
-from utils.generator import generate_random_password
 from utils.utils import set_current_password, get_current_password
 
 
